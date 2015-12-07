477e68b667294b61f4cccdc25adca6924b973f09
Alexander Pirsig <self@pirsig.net>
2015-12-07 22:45:26 +0100

    add cache control header

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 // Enable cors header
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cache-Control');
   next();
 });
 
